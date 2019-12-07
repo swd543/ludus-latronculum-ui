ca233a302b728762ee9a75a43e8ae440ba1e493f
Swapneel Datta <swd543@gmail.com>
2019-12-07 13:45:30 +0100

    Updates

---
+++ precache-manifest.a036006a9f781c953da53c6cc4585793.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a38adabb8bc266a1a7aa539b3ef7f72b",
+    "url": "/ludus-latronculum-ui/index.html"
+  },
+  {
+    "revision": "301509591c6aea29c91f",
+    "url": "/ludus-latronculum-ui/static/css/main.2d9a2103.chunk.css"
+  },
+  {
+    "revision": "ee7b0c4a097985f6f9f7",
+    "url": "/ludus-latronculum-ui/static/js/2.e4c748e9.chunk.js"
+  },
+  {
+    "revision": "301509591c6aea29c91f",
+    "url": "/ludus-latronculum-ui/static/js/main.91b86d0d.chunk.js"
+  },
+  {
+    "revision": "a22852fd155c901e0953",
+    "url": "/ludus-latronculum-ui/static/js/runtime-main.d7026624.js"
+  }
+]);
